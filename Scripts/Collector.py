73e3e9bf1d2a4d63467535d5890f4323d11ae26d
sarvari1378 <137143835+sarvari1378@users.noreply.github.com>
2023-10-04 16:08:23 +0330

    Update Collector.py

--- Scripts/Collector.py
+++ Scripts/Collector.py
@@ -1,6 +1,8 @@
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetHistoryRequest
 import re
+from datetime import datetime
+import pytz
 
 api_id = '26963557'
 api_hash = '70aed19a29d2321933d9c4f652534c0f'
@@ -28,7 +30,14 @@ async def main():
             vless_links.extend(links)
 
         with open(f'{channel_username}.txt', 'w') as f:
-            for link in vless_links:
-                f.write(f'{link}\n')
+            for i, link in enumerate(vless_links):
+                # Get current time in Iran/Tehran
+                tehran_tz = pytz.timezone('Asia/Tehran')
+                tehran_time = datetime.now(tehran_tz)
+                hour_minute = tehran_time.strftime('%H:%M')
+
+                # Replace all characters after '#' with '|Channel_Username|$Hour|X|'
+                modified_link = re.sub('#.*', f'|{hour_minute}|{i+1}|', link)
+                f.write(f'{modified_link}\n')
 
 client.loop.run_until_complete(main())
